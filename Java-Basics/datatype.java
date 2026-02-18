d8a4e04991400fef9a35af0ca04578423083cea9
Swarnima Chaurasia <swarichaurasia7@gmail.com>
2026-02-18 15:34:16 +0530

    Added user input

---
+++ Java-Basics/datatype.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class datatype {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("enter your age:");
+        int age = sc.nextInt();
+
+        System.out.println("hello " + name + ", you are "+ age + " years old.");
+
+        sc.close();
+    }
+}
